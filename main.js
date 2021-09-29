cd70ac2d36d9403759ce5f7154f98c2f31496ebd
ayudan11 <79744285+ayudan11@users.noreply.github.com>
2021-09-29 15:37:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,18 +11,29 @@ recognition.onresult=function run(event){
     console.log(event);
 
     var content=event.results[0][0].transcript;
-    console.log(content);
-
+    
     document.getElementById("textbox").innerHTML=content;
-    speak();
-}
+    console.log("content");
+    if(content=="take my selfie")
+    {
+        console.log("taking selfie ---");
+        speak();
+    }
+    }
 
 function speak(){
     var synth=window.speechSynthesis;
-    speak_data=document.getElementById("textbox").value;
+    speak_data="Taking your selfie in 5 seconds";
     var utterThis=new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+
+    },5000);
+    
 }
 
 Webcam.set({
@@ -33,4 +44,16 @@ Webcam.set({
 });
 camera=document.getElementById("camera");
 
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+ function save(){
+     link=document.getElementById("link");
+     image=document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
+ }
 
